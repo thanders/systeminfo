a8032cb6d5eee6b1509c4b2ecebd53109a173f81
thanders <thanders@thanders-MB>
2018-01-30 10:26:08 +0000

    my first commit. yay

--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
-'''
-Created on 08 January 2018
-@author: tom
-'''
-
 from setuptools import setup
 
-setup(name="systeminfo",
-      version="0.1",
-      decription="Basic system information for COMP30670",
-      url="",
-      author="Thomas Anderson",
-      author_email="thoma.anderson@ucdconnect.ie",
-      license="gpl3",
-      packages=['systeminfo'],
-      entrypoints={
-          'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
-          }
-      )
-      
+setup (name="systeminfo",
+       version="0.1",
+       description="Basic system information for COMP30670",
+       url="",
+       author="Thomas Anderson",
+       author="GPL3",
+       packages=['systeminfo'],
+       entry_points={
+           'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
+           }
+       )
